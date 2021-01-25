8d2b12c93c0bd77dd581c3757ed11dcc49173a8a
Aulia Sabri <aulia@localhost.localdomain>
2021-01-25 17:45:17 +0700

    commit for deploying

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App className="container" />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import FormStuff from "../components/FormStuff";
 
 const Home = () => {
   return (
-    <div className="home min-vh-100 pb-4">
+    <div className="home min-vh-100 pb-4 container">
       <h1 className="app-title py-5">My Stuff</h1>
       <Table />
       <FormStuff />
