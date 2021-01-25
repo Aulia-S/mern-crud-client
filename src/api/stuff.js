6672d57f47792b34cdd1939f01595adcfcfd7388
Aulia Sabri <aulia@localhost.localdomain>
2021-01-25 17:25:06 +0700

    commit for deploying

--- src/api/stuff.js
+++ src/api/stuff.js
@@ -1,31 +1,33 @@
 import axios from "axios";
 
+const URL = "https://mern-crud-server.herokuapp.com";
+
 export const createStuff = async (data) => {
-  const response = await axios.post("/api/stuff", data);
+  const response = await axios.post(`${URL}/api/stuff`, data);
 
   return response;
 };
 
 export const getStuffs = async () => {
-  const response = await axios.get("/api/stuff");
+  const response = await axios.get(`${URL}/api/stuff`);
 
   return response;
 };
 
 export const getStuff = async (stuffId) => {
-  const response = await axios.get(`/api/stuff/${stuffId}`);
+  const response = await axios.get(`${URL}/api/stuff/${stuffId}`);
 
   return response;
 };
 
 export const editStuff = async (data, id) => {
-  const response = await axios.put(`/api/stuff/${id}`, data);
+  const response = await axios.put(`${URL}/api/stuff/${id}`, data);
 
   return response;
 };
 
 export const deleteStuff = async (stuffId) => {
-  const response = await axios.delete(`/api/stuff/${stuffId}`);
+  const response = await axios.delete(`${URL}/api/stuff/${stuffId}`);
 
   return response;
 };
