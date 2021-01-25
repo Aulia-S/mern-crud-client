21f48657da4854342b1cc08149acc44471b90b58
Aulia Sabri <aulia@localhost.localdomain>
2021-01-25 17:51:11 +0700

    commit for deploying

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import FormStuff from "../components/FormStuff";
 
 const Home = () => {
   return (
-    <div className="home min-vh-100 pb-4 vw-100">
+    <div className="home min-vh-100 pb-4 min-vw-100">
       <h1 className="app-title py-5">My Stuff</h1>
       <Table />
       <FormStuff />
